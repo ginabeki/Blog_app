9768e3ae802cc112abb2b51c26121c75d9567d7b
ginabeki <ginabeki3@gmail.com>
2023-03-11 11:35:00 +0300

    Add api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,18 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
     resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create, :destroy]
+    end
   end
-end
+
+# API endpoints
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :update, :destroy, :show] do
+        resources :posts, only: [:index, :create, :update, :destroy, :show] do
+          resources :comments, only: [:index, :show]
+          resources :likes, only: [:index, :show]
+        end
+      end
+    end
+  end
 end
